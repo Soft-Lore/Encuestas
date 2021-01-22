95b60d11fbddd1875b5f36778ed796aa6bdd6e9b
RochaMoran <67056904+RochaMoran@users.noreply.github.com>
2021-01-22 13:23:22 -0600

    Conexion con la API de usuarios

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,7 +1,15 @@
+import { useEffect, useState } from 'react'
 import { Nav } from '../molecules/index'
 import { MySurveys, ButtonAddSurvey, IconAdd } from '../styled/Home'
+import axios from 'axios'
 
 const Home = () => {
+    const [users, setUsers] = useState()
+    useEffect(() => {
+        axios.get('/api/users')
+            .then(resp => setUsers(Object(resp).data.users))
+    }, [])
+
     return (
         <>
             <Nav />
@@ -10,6 +18,20 @@ const Home = () => {
                     Crear Nueva encuesta
                     <IconAdd />
                 </ButtonAddSurvey>
+                <div>
+                    <h1>Usuarios</h1>
+                    <ul>
+                        {
+                            users.map(m => <li>{m.name}</li>)
+                        }
+                    </ul>
+                    <h1>Id</h1>
+                    <ul>
+                        {
+                            users.map(m => <li>{m._id}</li>)
+                        }
+                    </ul>
+                </div>
             </MySurveys>
         </>
     )
