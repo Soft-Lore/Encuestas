e14d30c5ab72261c40d84403ce236475a69050d2
Nobody <moisesalex132@gmail.com>
2021-06-03 16:43:25 -0600

    ♻ REFACTOR: mensaje de email

--- API/src/helper/varify.email.js
+++ API/src/helper/varify.email.js
@@ -30,11 +30,11 @@ const sendEmail = async (email, subject, html) => {
 const getTemplate = (name, token) => {
     return `     
         <div style="margin-left: 35%; margin-right: 50%; width:400px" class="cont-message">
-        <img style="margin-left: 50px;" width="100px" src="https://avatars.githubusercontent.com/u/84889331?s=400&u=7d4de3840fd2b45c1c5a164a1ae74bdb60a7b51f&v=4" alt="">
-        <h2 style="margin-left: 20px;">Hola ${name} Somos Soft-Lore</h2>
-        <p style="margin-left: 20px;">Necesitamos validar tu dirección de email</p>
-        <div style="margin-left: 20px; margin-top: 20px; width: 200px; background-color: #90be6d; padding: 10px; border-radius: 10px; text-align: center" class="buttom">
-            <a style="text-decoration: none; color: white; text-transform: uppercase;" href="http://localhost:4000/api/user/confirm/${ token }">Confirmar cuenta</a>
+        <img style="margin-left: 160px;" width="100px" src="https://avatars.githubusercontent.com/u/84889331?s=400&u=7d4de3840fd2b45c1c5a164a1ae74bdb60a7b51f&v=4" alt="">
+        <h2 style="margin-left: 82px;">Hola ${name} Somos Soft-Lore</h2>
+        <p style="margin-left: 92px;">Necesitamos validar tu dirección de email</p>
+        <div style="margin-left: 100px; margin-top: 20px; width: 200px; background-color: #90be6d; padding: 10px; border-radius: 10px; text-align: center" class="buttom">
+            <a style="text-decoration: none; color: white; text-transform: uppercase;" href="http://localhost:4000/api/user/confirm/${token}">Confirmar cuenta</a>
         </div>
         </div>
       `;
